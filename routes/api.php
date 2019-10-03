3045f99178a2fa7d6b6cb939b52c9bee5cc3afc2
chadonsom <chadonsom@penguin>
2019-10-03 19:48:15 -0400

    Scaffolding
    - remove mix-manifest from git tracking
    - transfer to composition api
    - put in some api routes for 'old' budget info

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->group(function () {
+  Route::get('/old/checks', function (Request $request) {
+    
+  });
+  Route::get('/old/bills', function (Request $request) {
+    
+  });
+});
