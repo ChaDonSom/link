18113b7b60083ed8eb683bca407f4516cc44b6e3
ChaDonSom <chadonsom@gmail.com>
2019-11-13 14:20:55 -0500

    Add blank register page

--- resources/js/route/index.js
+++ resources/js/route/index.js
@@ -2,14 +2,16 @@ import { VueRouter } from '@js/bootstrap'
 
 import { auth } from '@route/guards'
 
-import Home    from '@pages/Home.vue'
-import Login   from '@pages/Login.vue'
-import Welcome from '@pages/Welcome.vue'
+import Home     from '@pages/Home.vue'
+import Login    from '@pages/Login.vue'
+import Welcome  from '@pages/Welcome.vue'
+import Register from '@pages/Register.vue'
 
 const routes = [
-  { path: '/',        component: Home,    props: true },
-  { path: '/login',   component: Login,   props: true },
-  { path: '/welcome', component: Welcome, props: true }
+  { path: '/',         component: Home,     props: true },
+  { path: '/login',    component: Login,    props: true },
+  { path: '/welcome',  component: Welcome,  props: true },
+  { path: '/register', component: Register, props: true },
 ]
 
 const router = new VueRouter({
