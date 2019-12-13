fc013b9fdd09ffb18255dcc560e11c5bf08461d2
Chase Phone <chadonsom@gmail.com>
2019-12-13 10:32:16 -0500

    Add nodes module

---
+++ resources/js/store/modules/nodes.js
@@ -0,0 +1,17 @@
+// Nodes Module
+import base from '@store/base'
+
+export default {
+  namespaced: true,
+  state: {
+  },
+  getters: {
+    ...base.getters,
+  },
+  mutations: {
+    ...base.mutations,
+  },
+  actions: {
+    ...base.actions,
+  }
+}
